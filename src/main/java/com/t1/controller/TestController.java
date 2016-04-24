d98f97f277c3a256d3f3fc876afa0aa328a32582
sam <amar19860330@gmail.com>
2016-04-24 21:17:34 +0800

    试试缓存

--- src/main/java/com/t1/controller/TestController.java
+++ src/main/java/com/t1/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.t1.controller;
 
 import com.t1.db.dao.NewsMapper;
+import com.t1.db.model.News;
 import com.t1.db.model.NewsExample;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  *
@@ -22,15 +24,26 @@ public class TestController {
 
 	@RequestMapping
     public String list(HttpServletRequest request, HttpServletResponse response){
-        int count = newsMapper.countByExample(new NewsExample());
-        //int count = 55;
-        count = 555;
+        NewsExample newsExample = new NewsExample();
+        System.out.println( newsExample.toString());
+        int count = newsMapper.countByExample(newsExample);
+
         request.setAttribute("count",""+count);
         return "test/t1";
     }
 
 	@RequestMapping(value = "/login")
     public String login(){
+        News news = new News();
+        news.setCategoryid(1);
+        news.setContent("ccccc");
+        news.setEditorid(1);
+        news.setTitle("222");
+        news.setTitlepic("ssss");
+        news.setStatus(2);
+        news.setStarttime( new Date());
+
+        newsMapper.insert(news );
         return "test/t2";
     }
 }
