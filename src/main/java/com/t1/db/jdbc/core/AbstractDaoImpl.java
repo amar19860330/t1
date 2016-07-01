979c1e74383b89c6bc5ba04feda56ca224501fff
sam <amar19860330@gmail.com>
2016-07-01 22:27:45 +0800

    jdbc 2

--- src/main/java/com/t1/db/jdbc/core/AbstractDaoImpl.java
+++ src/main/java/com/t1/db/jdbc/core/AbstractDaoImpl.java
@@ -3,18 +3,25 @@ package com.t1.db.jdbc.core;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 /**
  * Created by User on 2016/6/29.
  */
+//@Repository
 public abstract class AbstractDaoImpl<T> implements IBaseDao<T> {
 
     protected JdbcTemplate jdbcTemplate;
@@ -25,12 +32,10 @@ public abstract class AbstractDaoImpl<T> implements IBaseDao<T> {
 
     @SuppressWarnings("unchecked")
     public AbstractDaoImpl() {
-
         // 获取真实泛型数据类型
         Type t = getClass().getGenericSuperclass();
         ParameterizedType pt = (ParameterizedType) t;
         type = (Class) pt.getActualTypeArguments()[0];
-
     }
 
     public JdbcTemplate getJdbcTemplate() {
@@ -45,157 +50,62 @@ public abstract class AbstractDaoImpl<T> implements IBaseDao<T> {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<T> query(String sql)throws Exception{
-       return jdbcTemplate.queryForList(sql,type);
-        //return null ;//jdbcTemplate.queryForList(sql);
+    //***********************************************
+    //查询
+    public List<T> queryList(String sql, Object... params) throws Exception {
+        return jdbcTemplate.queryForList(sql, type, params);
     }
 
-
-
-    public String getTable_name() {
-        return table_name;
+    public List<T> queryList(String sql) throws Exception {
+        return jdbcTemplate.queryForList(sql, type);
     }
 
-    public void setTable_name(String table_name) {
-        this.table_name = table_name;
+    public Map<String,Object> queryMap(String sql) {
+        return jdbcTemplate.queryForMap(sql);
     }
 
-
-
-
-    @Override
-    public void create(T t) throws Exception {
-        if (t != null) {
-            try {
-                Field[] fields = t.getClass().getDeclaredFields();
-                String nameString = "";
-                String valueString = "";
-                String sql = "insert into " + table_name;
-                Object[] values = new Object[fields.length];
-                //设置SQL中的参数名和参数个数
-                for (int i = 0; i < fields.length; i++) {
-                    fields[i].setAccessible(true);//设置private属性可访问
-
-                    nameString += fields[i].getName() + ",";
-                    valueString += "?,";
-
-                    values[i] = fields[i].get(t);
-                }
-                //组织SQL语句
-                nameString = "(" + nameString.substring(0, nameString.length() - 1) + ")values";
-                valueString = "(" + valueString.substring(0, valueString.length() - 1) + ")";
-                sql = sql + nameString + valueString;
-
-                jdbcTemplate.update(sql, values);
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-            }
-        }
+    public Map<String,Object> queryMap(String sql,Object...params) {
+        return jdbcTemplate.queryForMap(sql,params);
     }
 
-    @Override
-    public T read(Integer id) throws Exception {
+    //删除类
+    public int delete(String sql) throws Exception {
+        return jdbcTemplate.update(sql);
+    }
 
-        final Class tempType = type;
+    public int delete(String sql, Object... param) throws Exception {
+        return jdbcTemplate.update(sql,param);
+    }
 
-        T t = jdbcTemplate.queryForObject(
-                "select * from " + table_name + " where id=?;", new Object[]{id},
-                new RowMapper<T>() {
-
-                    public T mapRow(java.sql.ResultSet rs, int arg1)
-                            throws SQLException {
-
-                        try {
-
-                            Object temp = tempType.newInstance();//实例化一个类用于获取field的值
-
-                            Field[] fields = tempType.getDeclaredFields();
-
-                            for (int i = 0; i < fields.length; i++) {
-                                fields[i].setAccessible(true);//设置field可访问
-                                fields[i].set(temp, rs.getObject(fields[i].getName()));//设置field的值
-                            }
-                            return (T) temp;
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            return null;
-                        }
+    //更新类
+    public int update(String sql) throws Exception {
+        return jdbcTemplate.update(sql);
+    }
+    public int update(String sql,Object...params) throws Exception {
+        return jdbcTemplate.update(sql,params);
+    }
+    //新增类
+    public int add(String sql) throws Exception {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                        return connection.prepareStatement(sql,new String[]{"id"});
                     }
-                });
-        return t;
+                }, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
-    @Override
-    public void update(T t) throws Exception {
-
-        if (t != null) {
-            try {
-                String sql = "update " + table_name + " set ";
-
-                Field[] fields = t.getClass().getDeclaredFields();
-
-                Object[] values = new Object[fields.length];
-
-                int index = 0;
-                //构造SQL语句
-                for (Field field : fields) {
-                    field.setAccessible(true);
-                    //判断是不是主键
-                    if (field.getName().equals("id")) {
-                        values[values.length - 1] = field.get(t);
-                    } else {
-                        sql += field.getName() + "=?,";
-                        values[index] = field.get(t);
-                        index++;
+    public int add(String sql, Object... params) throws Exception {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                        return connection.prepareStatement(sql);
                     }
-                }
-
-                sql = sql.substring(0, sql.length() - 1) + " where id=?";//为了去掉逗号
-
-                jdbcTemplate.update(sql, values);
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-            }
-        }
+                }, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
-    @Override
-    public void delete(Integer id) throws Exception {
-
-        jdbcTemplate.update("delete from " + table_name + " where id=?", new Object[]{id});
-    }
-
-    @Override
-    public List<T> listAll() throws Exception {
-
-        final Class tempType = type;
-
-        return jdbcTemplate.query("select id,startTime,stateTime,positionInfo,sourceInfo,destinationInfo from " + table_name + ";",
-                new RowMapper<T>() {
-
-                    @Override
-                    public T mapRow(ResultSet rs, int arg1) throws SQLException {
-
-                        try {
-
-                            Object temp = tempType.newInstance();//实例化一个类用于获取field的值
-
-                            Field[] fields = tempType.getDeclaredFields();
-
-                            for (int i = 0; i < fields.length; i++) {
-                                fields[i].setAccessible(true);//设置field可访问
-                                fields[i].set(temp, rs.getObject(fields[i].getName()));//设置field的值
-                            }
-                            return (T) temp;
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            return null;
-                        }
-                    }
-                });
-    }
+    //***********************************************************
 }
--- src/main/java/com/t1/db/jdbc/core/IBaseDao.java
+++ src/main/java/com/t1/db/jdbc/core/IBaseDao.java
@@ -1,46 +1,36 @@
 package com.t1.db.jdbc.core;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by User on 2016/6/29.
  */
 public interface IBaseDao<T> {
-    public void create(T t) throws Exception;
-    public T read(Integer id) throws Exception;
-    public void delete(Integer id) throws Exception;
-    public List<T> listAll() throws Exception;
 
+    //查询类
+    public List<T> queryList(String sql, Object... params) throws Exception;
 
+    public List<T> queryList(String sql) throws Exception;
 
+    public Map<String, Object> queryMap(String sql);
 
-
-
-
-
-    //查询
-    public List<T> query(String sql,Object... params)throws Exception;
-    public List<T> query(T t,Object... params)throws Exception;
-    public int count(String sql,Object... params)throws Exception;
-
-
-    public List<T> query(String sql)throws Exception;
-    public List<T> query(T t)throws Exception;
-    public int count(String sql)throws Exception;
-
-
+    public Map<String, Object> queryMap(String sql, Object... params);
 
     //删除类
     public int delete(String sql) throws Exception;
 
-    //更新类
-    public int update(String sql)throws Exception;
+    public int delete(String sql, Object... param) throws Exception;
 
-    public void update(T t) throws Exception;
+    //更新类
+    public int update(String sql) throws Exception;
+
+    public int update(String sql, Object... params) throws Exception;
 
     //新增类
     public int add(T t) throws Exception;
 
+    public int add(String sql) throws Exception;
 
-
+    public int add(String sql,Object...params) throws Exception;
 }
